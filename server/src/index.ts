f9b7b46bebf3c6bf19907599053a03c91ea0db1c
Tuy Lv <levantuy.it@gmail.com>
2026-06-03 20:07:51 +0700

    x

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,6 @@
 import { env } from "./config/env.js";
 import { logger } from "./config/logger.js";
-import { createApp } from "./app.js";
-
-const app = createApp();
+import app from "./app.js";
 
 app.listen(env.PORT, () => {
   logger.info({ port: env.PORT }, "Swap API listening");
